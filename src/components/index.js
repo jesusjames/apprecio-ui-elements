26e24b63e74732dc38bcbbd5a8eff58462ce5e4c
alexalemanhdz <alex.aleman.public@gmail.com>
2022-02-27 17:57:18 -0600

    Working on legacy Wrapper component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 export { default as Button } from './Button/Button';
 export { default as Alert } from './Alert/Alert';
 export { default as Input } from './Input/Input';
+
+// Legacy components
+
+export { default as Wrapper } from './Wrapper/Wrapper';
