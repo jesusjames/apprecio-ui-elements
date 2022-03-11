436d1eb7c41f90b139209a34eba2e3c41fd25838
alexalemanhdz <alex.aleman.public@gmail.com>
2022-03-10 22:46:05 -0600

    Added basic structure fro Summary

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,11 +1,45 @@
+import PropTypes from 'prop-types';
+
 import { SummaryStyled } from './style';
 
 const Summary = ({
-  ...rest,
-}) => {
-  return (
-    <SummaryStyled {...rest} />
-  );
+  count,
+  date,
+  expenseTotal,
+  incomeTotal,
+  ...rest
+}) => (
+  <SummaryStyled {...rest}>
+    <div className="header">
+      <span className="date">{date}</span>
+      <button className="count" type="button">
+        {count}
+        {' '}
+        Transacciones
+      </button>
+    </div>
+    <div className="income-total">
+      <span className="income total">
+        $
+        {incomeTotal}
+      </span>
+      <span className="label">Ingresos</span>
+    </div>
+    <div className="expense-total">
+      <span className="expense total">
+        $
+        {expenseTotal}
+      </span>
+      <span className="label">Gastos</span>
+    </div>
+  </SummaryStyled>
+);
+
+Summary.propTypes = {
+  count: PropTypes.number.isRequired,
+  date: PropTypes.string.isRequired,
+  expenseTotal: PropTypes.number.isRequired,
+  incomeTotal: PropTypes.number.isRequired,
 };
 
 export default Summary;
